e1f3962820ad383038f81c9421d0a1f41070a685
Corey H <c.l.halicke@gmail.com>
2018-07-03 17:23:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,47 +12,21 @@ const START = document.getElementById('start')
 
 var gameInterval = null
 
-/**
- * Be aware of what's above this line,
- * but all of your work should happen below.
- */
-
 function checkCollision(rock) {
-  // implement me!
-  // use the comments below to guide you!
   const top = positionToInteger(rock.style.top);
 
-  // rocks are 20px high
-  // DODGER is 20px high
-  // GAME_HEIGHT - 20 - 20 = 360px;
   if (top > 360) {
     const dodgerLeftEdge = positionToInteger(DODGER.style.left);
-
-    // FIXME: The DODGER is 40 pixels wide -- how do we get the right edge?
     const dodgerRightEdge = dodgerLeftEdge + 40;
 
     const rockLeftEdge = positionToInteger(rock.style.left);
-
-    // FIXME: The rock is 20 pixel's wide -- how do we get the right edge?
-// changed from: const rockRightEdge = 0 
     const rockRightEdge = rockLeftEdge + 20;
 
-// changed from: if (false) { return true }
     if (
       (rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge)|| 
       (rockLeftEdge >= dodgerLeftEdge && rockLeftEdge <= dodgerRightEdge)||
       (rockLeftEdge <= dodgerRightEdge && rockRightEdge >= dodgerRightEdge)) {
       return true;
-              /**
-               * Think about it -- what's happening here?
-               * There's been a collision if one of three things is true:
-               * 1. The rock's left edge is < the DODGER's left edge,
-               *    and the rock's right edge is > the DODGER's left edge;
-               * 2. The rock's left edge is > the DODGER's left edge,
-               *    and the rock's right edge is < the DODGER's right edge;
-               * 3. The rock's left edge is < the DODGER's right edge,
-               *    and the rock's right edge is > the DODGER's right edge
-               */
     }
   }
 }
@@ -63,7 +37,6 @@ function createRock(x) {
   rock.className = 'rock'
   rock.style.left = `${x}px`
 
-  // Hmmm, why would we have used `var` here?
   var top = 0
 
   rock.style.top = top
@@ -72,21 +45,6 @@ function createRock(x) {
    * Now that we have a rock, we'll need to append
    * it to GAME and move it downwards.
    */
-   
-   
-// added step function
-  function step() {
-    el.style.top = `${top += 2}px`
- 
-    if (top < 200) {
-      window.requestAnimationFrame(step)
-    }
-  }
- 
-  window.requestAnimationFrame(step)
-}
-
-
 
 
   /**
